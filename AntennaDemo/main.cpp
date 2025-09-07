061b7a4273ef43eabea707ce584c0286a4201cd9
igor <igor.240340@gmail.com>
2025-09-08 01:57:23 +0700

    Integrate TGUI

--- AntennaDemo/main.cpp
+++ AntennaDemo/main.cpp
@@ -4,9 +4,12 @@
 #include <format>
 #include <random>
 #include <algorithm>
-#include <chrono>
 
 #include <SFML/Graphics.hpp>
+
+#include "TGUI/TGUI.hpp"
+#include "TGUI/Backend/SFML-Graphics.hpp"
+#include "TGUI/Core.hpp"
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
@@ -17,19 +20,26 @@
 
 #include "graphics.h"
 
+constexpr float deg_to_rad = static_cast<float>(std::numbers::pi / 180.0);
+constexpr float rad_to_deg = 1.0f / deg_to_rad;
+
+struct RotationAngles {
+	float yaw_angle_rad = 0.0f;
+	float roll_angle_rad = 0.0f;
+	float pitch_angle_rad = 0.0f;
+};
+
+void build_ui(tgui::Gui& gui, RotationAngles& rot_angles);
+
 void load_model(std::string model_path, std::vector<Polygon>& polygons);
 void rasterize_polygons_flat_shaded_ortho(const std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const Mat4f& transform);
 void rasterize_polygons_wireframe_ortho(const std::vector<Polygon>& polygons, Framebuffer& framebuffer, const Mat4f& transform);
 
 void draw_node0(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer);
-void draw_node1(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer);
-void draw_node2(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer);
-void draw_node3(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer);
-void draw_node4(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer);
-
-sf::Color get_random_color();
-
-using high_res_clock = std::chrono::high_resolution_clock;
+void draw_node1(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles);
+void draw_node2(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles);
+void draw_node3(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles);
+void draw_node4(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles);
 
 int main() {
 	constexpr int w = 800;
@@ -41,16 +51,13 @@ int main() {
 		return 1;
 	}
 
-	sf::Text fps_text;
-	fps_text.setString("fps: 0");
-	fps_text.setFont(font);
-	fps_text.setCharacterSize(16);
-	fps_text.setFillColor(sf::Color::White);
-	fps_text.setPosition(5.0f, 5.0f);
-
 	sf::RenderWindow window(sf::VideoMode(w, h), "Antenna Demo");
 	window.setFramerateLimit(0);
 	window.setVerticalSyncEnabled(true);
+
+	tgui::Gui gui{ window };
+	RotationAngles rot_angles;
+	build_ui(gui, rot_angles);
 
 	sf::Texture texture;
 	if (!texture.create(w, h)) {
@@ -79,41 +86,80 @@ int main() {
 	load_model(node3_path, node3_polygons);
 	load_model(node4_path, node4_polygons);
 
-	auto measure_start = high_res_clock::now();
-	int frame_count = 0;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
+			gui.handleEvent(event);
+
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
-		clear_framebuffer(sf::Color::Blue, framebuffer);
+		clear_framebuffer(sf::Color{ 0x3E92CC }, framebuffer);
 		clear_z_buffer(1.0f, z_buffer);
 
 		draw_node0(node0_polygons, light, framebuffer, z_buffer);
-		draw_node1(node1_polygons, light, framebuffer, z_buffer);
-		draw_node2(node2_polygons, light, framebuffer, z_buffer);
-		draw_node3(node3_polygons, light, framebuffer, z_buffer);
-		draw_node4(node4_polygons, light, framebuffer, z_buffer);
+		draw_node1(node1_polygons, light, framebuffer, z_buffer, rot_angles);
+		draw_node2(node2_polygons, light, framebuffer, z_buffer, rot_angles);
+		draw_node3(node3_polygons, light, framebuffer, z_buffer, rot_angles);
+		draw_node4(node4_polygons, light, framebuffer, z_buffer, rot_angles);
 
 		texture.update(framebuffer.rgba_array.data());
 
 		window.clear();
 		window.draw(sprite);
-		window.draw(fps_text);
-		window.display();
 
-		std::chrono::duration<float> elapsed_seconds = high_res_clock::now() - measure_start;
-		++frame_count;
-		if (elapsed_seconds.count() >= 1.0f) {
-			fps_text.setString(std::format("fps: {}", static_cast<int>(frame_count / elapsed_seconds.count())));
-			frame_count = 0;
-			measure_start = high_res_clock::now();
-		}
+		gui.draw();
+
+		window.display();
 	}
 
 	return 0;
+}
+
+void build_ui(tgui::Gui& gui, RotationAngles& rot_angles) {
+	tgui::VerticalLayout::Ptr layout = tgui::VerticalLayout::create();
+	layout->setPosition(10.0f, 10.0f);
+	layout->setSize(100, 150);
+	gui.add(layout);
+
+	tgui::EditBoxSlider::Ptr yaw_rot_slider = tgui::EditBoxSlider::create();
+	yaw_rot_slider->setMinimum(0.0f);
+	yaw_rot_slider->setMaximum(360.0f);
+	yaw_rot_slider->setStep(0.1f);
+	yaw_rot_slider->setValue(0.0f);
+	yaw_rot_slider->setDecimalPlaces(1);
+	yaw_rot_slider->setTextAlignment(tgui::HorizontalAlignment::Center);
+
+	tgui::EditBoxSlider::Ptr roll_rot_slider = tgui::EditBoxSlider::create();
+	roll_rot_slider->setMinimum(-180.0f);
+	roll_rot_slider->setMaximum(180.0f);
+	roll_rot_slider->setStep(0.1f);
+	roll_rot_slider->setValue(0.0f);
+	roll_rot_slider->setDecimalPlaces(3);
+	roll_rot_slider->setTextAlignment(tgui::HorizontalAlignment::Center);
+
+	tgui::EditBoxSlider::Ptr pitch_rot_slider = tgui::EditBoxSlider::create();
+	pitch_rot_slider->setMinimum(-90.0f);
+	pitch_rot_slider->setMaximum(90.0f);
+	pitch_rot_slider->setStep(0.1f);
+	pitch_rot_slider->setValue(0.0f);
+	pitch_rot_slider->setDecimalPlaces(3);
+	pitch_rot_slider->setTextAlignment(tgui::HorizontalAlignment::Center);
+
+	layout->add(yaw_rot_slider);
+	layout->add(roll_rot_slider);
+	layout->add(pitch_rot_slider);
+
+	yaw_rot_slider->onValueChange([&rot_angles](float yaw_angle_deg) {
+		rot_angles.yaw_angle_rad = yaw_angle_deg * deg_to_rad;
+		});
+	roll_rot_slider->onValueChange([&rot_angles](float roll_angle_deg) {
+		rot_angles.roll_angle_rad = roll_angle_deg * deg_to_rad;
+		});
+	pitch_rot_slider->onValueChange([&rot_angles](float pitch_angle_deg) {
+		rot_angles.pitch_angle_rad = pitch_angle_deg * deg_to_rad;
+		});
 }
 
 void load_model(std::string model_path, std::vector<Polygon>& polygons) {
@@ -149,7 +195,6 @@ void load_model(std::string model_path, std::vector<Polygon>& polygons) {
 				attrib.vertices[3 * index2.vertex_index + 2]
 			} };
 
-			//polygons.push_back(Polygon{ { v0, v1, v2 }, get_random_color() });
 			polygons.push_back(Polygon{ { v0, v1, v2 }, sf::Color::White });
 		}
 	}
@@ -227,68 +272,52 @@ void rasterize_polygons_wireframe_ortho(const std::vector<Polygon>& polygons, Fr
 
 void draw_node0(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer) {
 	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f, 0.0f });
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * static_cast<float>(std::numbers::pi / 180.0));
+	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
+	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
 
 	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * translation);
 }
 
-void draw_node1(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer) {
+void draw_node1(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles) {
 	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f + 0.5f, 0.0f });
-	const Mat4f base_rot = Mat4f::create_rotation_y(0.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * static_cast<float>(std::numbers::pi / 180.0));
+	const Mat4f yaw = Mat4f::create_rotation_y(rot_angles.yaw_angle_rad);
+	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
+	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
 
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * base_rot * translation);
+	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw * translation);
 }
 
-void draw_node2(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer) {
+void draw_node2(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles) {
 	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f + 0.5f, 0.0f });
-	const Mat4f base_rot = Mat4f::create_rotation_y(0.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * static_cast<float>(std::numbers::pi / 180.0));
+	const Mat4f yaw = Mat4f::create_rotation_y(rot_angles.yaw_angle_rad);
+	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
+	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
+	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
 
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * base_rot * translation * tilt);
+	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw * translation * tilt);
 }
 
-void draw_node3(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer) {
+void draw_node3(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles) {
 	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, 2.7f, -0.8f });
-	const Mat4f translation2 = Mat4f::create_translation(Vec3f{ 0.0f, -2.0+0.5f, 0.0f });
-	const Mat4f base_rot = Mat4f::create_rotation_y(0.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f roll = Mat4f::create_rotation_z(30.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * static_cast<float>(std::numbers::pi / 180.0));
+	const Mat4f translation2 = Mat4f::create_translation(Vec3f{ 0.0f, -2.0 + 0.5f, 0.0f });
+	const Mat4f yaw = Mat4f::create_rotation_y(rot_angles.yaw_angle_rad);
+	const Mat4f roll = Mat4f::create_rotation_z(rot_angles.roll_angle_rad);
+	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
+	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
+	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
 
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * base_rot * translation2 * tilt * translation * roll);
+	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw * translation2 * tilt * translation * roll);
 }
 
-void draw_node4(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer) {
-	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, 0.0f + 2.7f, -1.3f });
-	const Mat4f translation2 = Mat4f::create_translation(Vec3f{ 0.0f, -2.0+0.5f, 0.0f });
-	const Mat4f base_rot = Mat4f::create_rotation_y(0.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f roll = Mat4f::create_rotation_z(30.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f pitch = Mat4f::create_rotation_x(0.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * static_cast<float>(std::numbers::pi / 180.0));
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * static_cast<float>(std::numbers::pi / 180.0));
+void draw_node4(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const RotationAngles& rot_angles) {
+	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, 2.7f, -1.3f });
+	const Mat4f translation2 = Mat4f::create_translation(Vec3f{ 0.0f, -2.0 + 0.5f, 0.0f });
+	const Mat4f yaw_rot = Mat4f::create_rotation_y(rot_angles.yaw_angle_rad);
+	const Mat4f roll = Mat4f::create_rotation_z(rot_angles.roll_angle_rad);
+	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
+	const Mat4f pitch = Mat4f::create_rotation_x(rot_angles.pitch_angle_rad);
+	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
+	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
 
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * base_rot * translation2 * tilt * translation * roll * pitch);
-}
-
-sf::Color get_random_color() {
-	static std::random_device rd;
-	static std::mt19937 gen(rd());
-	//static std::normal_distribution<float> dist(128.0f, 48.0f);
-	static std::uniform_int_distribution dist(0, 255);
-
-	/*sf::Uint8 r = static_cast<sf::Uint8>(std::clamp(static_cast<int>(std::round(dist(gen))), 0, 255));
-	sf::Uint8 g = static_cast<sf::Uint8>(std::clamp(static_cast<int>(std::round(dist(gen))), 0, 255));
-	sf::Uint8 b = static_cast<sf::Uint8>(std::clamp(static_cast<int>(std::round(dist(gen))), 0, 255));*/
-
-	sf::Uint8 r = static_cast<sf::Uint8>(dist(gen));
-	sf::Uint8 g = static_cast<sf::Uint8>(dist(gen));
-	sf::Uint8 b = static_cast<sf::Uint8>(dist(gen));
-	return sf::Color(r, g, b);
+	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw_rot * translation2 * tilt * translation * roll * pitch);
 }
