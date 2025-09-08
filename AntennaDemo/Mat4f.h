91db55724889145b7f75be3d2a1402b93172b24f
igor <igor.240340@gmail.com>
2025-09-09 05:32:23 +0700

    Make release build

--- AntennaDemo/Mat4f.h
+++ AntennaDemo/Mat4f.h
@@ -16,7 +16,6 @@ public:
 
     static Mat4f create_identity();
 
-    static Mat4f create_look_at(const Vec3f& pos, const Vec3f& look_dir, const Vec3f& up);
     static Mat4f create_perspective(float fov_vert_rad, float aspect_ratio, float near, float far);
     static Mat4f create_ortho(float left, float right, float bottom, float top, float near, float far);
     static Mat4f create_viewport(int w, int h);
--- AntennaDemo/main.cpp
+++ AntennaDemo/main.cpp
@@ -1,9 +1,6 @@
 ﻿#include <iostream>
-#include <ranges>
 #include <numbers>
 #include <format>
-#include <random>
-#include <algorithm>
 
 #include <SFML/Graphics.hpp>
 
@@ -22,53 +19,59 @@
 
 constexpr float deg_to_rad = static_cast<float>(std::numbers::pi / 180.0);
 constexpr float rad_to_deg = 1.0f / deg_to_rad;
-
-struct InputAngles {
-	const float fixed_elevation_rad = 10.0f * deg_to_rad;
-
-	float yaw_angle_rad = 0.0f;
-	float roll_angle_rad = 0.0f;
-	float pitch_angle_rad = 0.0f;
-};
+constexpr float pi_half = static_cast<float>(std::numbers::pi / 2.0);
+constexpr float pi_two = static_cast<float>(std::numbers::pi * 2.0);
 
 struct AntennaOrientation {
-	float azimuth = 0.0f;
-	float elevation = 0.0f;
+	float azimuth_deg = 0.0f;
+	float elevation_deg = 0.0f;
 };
 
-void build_ui(tgui::Gui& gui, InputAngles& input_angles);
+struct TransformMatrices {
+	Mat4f camera_rot;
+	Mat4f transform_node0;
+	Mat4f tilt; // Небольшой фиксированный наклон.
+	Mat4f translation; // Сдвиг всех узлов, кроме основания.
+	Mat4f translation_node3;
+	Mat4f translation_node4;
+
+	// Обновляются из UI.
+	Mat4f yaw;
+	Mat4f roll;
+	Mat4f pitch;
+
+	// Перестраиваются, когда пользователь изменил хотя бы одно значение в UI.
+	Mat4f yaw_trans_cam;
+	Mat4f tilt_yaw_trans_cam;
+	Mat4f pitch_roll;
+
+	// Устанавливается в true, когда пользователь поменял в UI хотя бы одно значение.
+	// Это нужно, чтобы не перестраивать матрицы в каждом кадре.
+	// При первом запуске структура инициализируется новыми значениями, поэтому true.
+	bool changed = true;
+};
 
 void load_model(std::string model_path, std::vector<Polygon>& polygons);
+void init_transforms(TransformMatrices& transforms);
+void build_ui(tgui::Gui& gui, TransformMatrices& transforms);
+void draw_antenna(std::vector<std::vector<Polygon>> antenna_mesh, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const TransformMatrices& transforms);
 void rasterize_polygons_flat_shaded_ortho(const std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const Mat4f& transform);
-void rasterize_polygons_wireframe_ortho(const std::vector<Polygon>& polygons, Framebuffer& framebuffer, const Mat4f& transform);
-
-void recalc_antenna_orientation(const InputAngles& input_angles, AntennaOrientation& antenna_orientation);
-
-void draw_node0(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer);
-void draw_node1(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles);
-void draw_node2(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles);
-void draw_node3(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles);
-void draw_node4(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles);
+void recalc_antenna_orientation(const TransformMatrices& transforms, AntennaOrientation& antenna_orientation);
 
 int main() {
 	constexpr int w = 800;
 	constexpr int h = 600;
 
-	sf::Font font;
-	if (!font.loadFromFile("assets/fira_mono.ttf")) {
-		std::cout << "SFML: font.loadFromFile() failed.\n";
-		return 1;
-	}
-
-	sf::RenderWindow window(sf::VideoMode(w, h), "Antenna Demo");
+	sf::RenderWindow window(sf::VideoMode(w, h), "Antenna Demo", sf::Style::Titlebar | sf::Style::Close);
 	window.setFramerateLimit(0);
 	window.setVerticalSyncEnabled(true);
 
-	AntennaOrientation antenna_orientation;
-	InputAngles input_angles;
-
 	tgui::Gui gui{ window };
-	build_ui(gui, input_angles);
+
+	AntennaOrientation antenna_orientation;
+	TransformMatrices transforms;
+	init_transforms(transforms);
+	build_ui(gui, transforms);
 
 	sf::Texture texture;
 	if (!texture.create(w, h)) {
@@ -81,21 +84,10 @@ int main() {
 	Framebuffer framebuffer{ w, h, std::vector<sf::Uint8>(w * h * 4) };
 	ZBuffer z_buffer{ w, h, std::vector<float>(w * h) };
 
-	std::vector<Polygon> node0_polygons;
-	std::vector<Polygon> node1_polygons;
-	std::vector<Polygon> node2_polygons;
-	std::vector<Polygon> node3_polygons;
-	std::vector<Polygon> node4_polygons;
-	std::string node0_path = "assets/antenna/node_0.obj";
-	std::string node1_path = "assets/antenna/node_1.obj";
-	std::string node2_path = "assets/antenna/node_2.obj";
-	std::string node3_path = "assets/antenna/node_3.obj";
-	std::string node4_path = "assets/antenna/node_4.obj";
-	load_model(node0_path, node0_polygons);
-	load_model(node1_path, node1_polygons);
-	load_model(node2_path, node2_polygons);
-	load_model(node3_path, node3_polygons);
-	load_model(node4_path, node4_polygons);
+	// Загружаем меш антенны из отдельных файлов.
+	std::vector<std::vector<Polygon>> antenna_mesh(5);
+	for (int i = 0; i < 5; ++i)
+		load_model(std::format("assets/antenna/node_{}.obj", i), antenna_mesh[i]);
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -109,20 +101,16 @@ int main() {
 		clear_framebuffer(sf::Color{ 0x3e92cc }, framebuffer);
 		clear_z_buffer(1.0f, z_buffer);
 
-		draw_node0(node0_polygons, light, framebuffer, z_buffer);
-		draw_node1(node1_polygons, light, framebuffer, z_buffer, input_angles);
-		draw_node2(node2_polygons, light, framebuffer, z_buffer, input_angles);
-		draw_node3(node3_polygons, light, framebuffer, z_buffer, input_angles);
-		draw_node4(node4_polygons, light, framebuffer, z_buffer, input_angles);
+		draw_antenna(antenna_mesh, light, framebuffer, z_buffer, transforms);
 
-		recalc_antenna_orientation(input_angles, antenna_orientation);
+		recalc_antenna_orientation(transforms, antenna_orientation);
 		tgui::Label::Ptr azimuth_label = gui.get<tgui::Label>("azimuth_label");
 		if (azimuth_label)
-			azimuth_label->setText(std::format("Azimuth:\n{}", antenna_orientation.azimuth));
+			azimuth_label->setText(std::format("Azimuth:\n{}", antenna_orientation.azimuth_deg));
 
 		tgui::Label::Ptr elevation_label = gui.get<tgui::Label>("elevation_label");
 		if (elevation_label)
-			elevation_label->setText(std::format("Elevation:\n{}", antenna_orientation.elevation));
+			elevation_label->setText(std::format("Elevation:\n{}", antenna_orientation.elevation_deg));
 
 		texture.update(framebuffer.rgba_array.data());
 
@@ -130,6 +118,13 @@ int main() {
 		window.draw(sprite);
 
 		gui.draw();
+		// Пересчитываем матрицы, если пользователь изменил значения в UI.
+		if (transforms.changed) {
+			transforms.yaw_trans_cam = transforms.camera_rot * transforms.translation * transforms.yaw;
+			transforms.tilt_yaw_trans_cam = transforms.yaw_trans_cam * transforms.tilt;
+			transforms.pitch_roll = transforms.roll * transforms.pitch;
+			transforms.changed = false;
+		}
 
 		window.display();
 	}
@@ -137,10 +132,10 @@ int main() {
 	return 0;
 }
 
-void build_ui(tgui::Gui& gui, InputAngles& input_angles) {
+void build_ui(tgui::Gui& gui, TransformMatrices& transforms) {
 	tgui::VerticalLayout::Ptr layout = tgui::VerticalLayout::create();
 	layout->setPosition(10.0f, 15.0f);
-	layout->setSize(140, 220);
+	layout->setSize(150, 220);
 	layout->getRenderer()->setSpaceBetweenWidgets(5);
 
 	tgui::EditBoxSlider::Ptr yaw_rot_slider = tgui::EditBoxSlider::create();
@@ -160,21 +155,29 @@ void build_ui(tgui::Gui& gui, InputAngles& input_angles) {
 	roll_rot_slider->setTextAlignment(tgui::HorizontalAlignment::Center);
 
 	tgui::EditBoxSlider::Ptr pitch_rot_slider = tgui::EditBoxSlider::create();
-	pitch_rot_slider->setMinimum(-90.0f);
-	pitch_rot_slider->setMaximum(90.0f);
+	pitch_rot_slider->setMinimum(-45.0f);
+	pitch_rot_slider->setMaximum(45.0f);
 	pitch_rot_slider->setStep(0.1f);
 	pitch_rot_slider->setValue(0.0f);
 	pitch_rot_slider->setDecimalPlaces(1);
 	pitch_rot_slider->setTextAlignment(tgui::HorizontalAlignment::Center);
 
-	yaw_rot_slider->onValueChange([&input_angles](float yaw_angle_deg) {
-		input_angles.yaw_angle_rad = yaw_angle_deg * deg_to_rad;
+	yaw_rot_slider->onValueChange([&transforms](float yaw_angle_deg) {
+		// NOTE: В нашей мировой СК положительный Z смотрит в камеру, на нас.
+		// При загрузке меша, антенна смотрит в направлении -Z, это направление - виртуальный географический север.
+		// Положительное направление вращение - по часовой стрелке.
+		transforms.yaw = Mat4f::create_rotation_y(-yaw_angle_deg * deg_to_rad);
+		transforms.changed = true;
 		});
-	roll_rot_slider->onValueChange([&input_angles](float roll_angle_deg) {
-		input_angles.roll_angle_rad = roll_angle_deg * deg_to_rad;
+	roll_rot_slider->onValueChange([&transforms](float roll_angle_deg) {
+		// NOTE: Положительное направление вращения конвертера - по часовой стрелке,
+		// когда рефлектор отвернут от нас, а конвертер, соответственно, смотрит нам в лицо.
+		transforms.roll = Mat4f::create_rotation_z(-roll_angle_deg * deg_to_rad);
+		transforms.changed = true;
 		});
-	pitch_rot_slider->onValueChange([&input_angles](float pitch_angle_deg) {
-		input_angles.pitch_angle_rad = pitch_angle_deg * deg_to_rad;
+	pitch_rot_slider->onValueChange([&transforms](float pitch_angle_deg) {
+		transforms.pitch = Mat4f::create_rotation_x(pitch_angle_deg * deg_to_rad);
+		transforms.changed = true;
 		});
 
 	layout->add(yaw_rot_slider);
@@ -240,11 +243,12 @@ void load_model(std::string model_path, std::vector<Polygon>& polygons) {
 }
 
 void rasterize_polygons_flat_shaded_ortho(const std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const Mat4f& transform) {
-	const float ratio = static_cast<float>(framebuffer.w) / framebuffer.h;
-	const float proj_plane_w = 15.0f;
-	const float proj_plane_h = proj_plane_w / ratio;
-	const Mat4f proj = Mat4f::create_ortho(-proj_plane_w / 2, proj_plane_w / 2, -proj_plane_h / 2, proj_plane_h / 2, 0.1f, 10.0f);
-	const Mat4f viewport = Mat4f::create_viewport(framebuffer.w, framebuffer.h);
+	// В нашем случае размеры окна никогда не меняются в рантайме.
+	const static float ratio = static_cast<float>(framebuffer.w) / framebuffer.h;
+	const static float proj_plane_w = 15.0f;
+	const static float proj_plane_h = proj_plane_w / ratio;
+	const static Mat4f proj = Mat4f::create_ortho(-proj_plane_w / 2.0f, proj_plane_w / 2.0f, -proj_plane_h / 2.0f, proj_plane_h / 2.0f, 0.1f, 10.0f);
+	const static Mat4f viewport = Mat4f::create_viewport(framebuffer.w, framebuffer.h);
 
 	for (Polygon polygon : polygons) {
 		for (Vertex& vertex : polygon.vertices) {
@@ -281,87 +285,47 @@ void rasterize_polygons_flat_shaded_ortho(const std::vector<Polygon>& polygons, 
 	}
 }
 
-void rasterize_polygons_wireframe_ortho(const std::vector<Polygon>& polygons, Framebuffer& framebuffer, const Mat4f& transform) {
-	const float ratio = static_cast<float>(framebuffer.w) / framebuffer.h;
-	const float proj_plane_w = 15.0f; // [-7.5, 7.5].
-	const float proj_plane_h = proj_plane_w / ratio; // Требуем, чтобы proj_plane_w / proj_plane_h = w / h.
-	const Mat4f proj = Mat4f::create_ortho(-proj_plane_w / 2, proj_plane_w / 2, -proj_plane_h / 2, proj_plane_h / 2, 0.1f, 10.0f);
-	const Mat4f viewport = Mat4f::create_viewport(framebuffer.w, framebuffer.h);
-
-	for (Polygon polygon : polygons) {
-		for (Vertex& vertex : polygon.vertices) {
-			const Vec4f pos{ vertex.pos };
-			vertex.pos = transform * pos;
-		}
-
-		std::vector<Vertex> vertices_screen;
-		for (const auto& vertex : polygon.vertices) {
-			Vec4f pos{ vertex.pos };
-
-			Vec4f pos_clip = proj * pos;
-			Vec4f pos_ndc = pos_clip / pos_clip.w; // NOTE: В случае ортографической проекции нет необходимости в перспективном делении.
-			Vec4f pos_screen = viewport * pos_ndc;
-
-			vertices_screen.push_back(Vertex{ Vec3f{pos_screen} });
-		}
-		Polygon polygon_screen{ {vertices_screen[0], vertices_screen[1], vertices_screen[2]} };
-		draw_polygon_wireframe(polygon_screen, framebuffer);
-	}
+void init_transforms(TransformMatrices& transforms) {
+	transforms.camera_rot = Mat4f::create_rotation_x(20.0f * deg_to_rad) * Mat4f::create_rotation_y(60.0f * deg_to_rad);
+	transforms.transform_node0 = transforms.camera_rot * Mat4f::create_translation(Vec3f{ 0.0f, -2.0f, 0.0f });
+	transforms.tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
+	transforms.translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f + 0.5f, 0.0f }); // Для всех узлов, кроме основания.
+	transforms.translation_node3 = Mat4f::create_translation(Vec3f{ 0.0f, 2.7f, -0.8f });
+	transforms.translation_node4 = Mat4f::create_translation(Vec3f{ 0.0f, 2.7f, -1.3f });
 }
 
-void recalc_antenna_orientation(const InputAngles& input_angles, AntennaOrientation& antenna_orientation) {
-	antenna_orientation.azimuth += 0.1f;
-	antenna_orientation.elevation += 0.1f;
+void recalc_antenna_orientation(const TransformMatrices& transforms, AntennaOrientation& antenna_orientation) {
+	static constexpr float epsilon = 1e-06f;
+	static const Vec4f reflector_dir_local{ 0.0f, 0.0f, -1.0f };
+
+	const Mat4f& pitch_roll = transforms.pitch_roll;
+	const Mat4f& tilt = transforms.tilt;
+	const Mat4f& yaw = transforms.yaw;
+
+	Vec3f dir_world{ yaw * tilt * pitch_roll * reflector_dir_local };
+	Vec3f dir_world_proj_ground{ dir_world.x, 0.0f, dir_world.z };
+	float dot_prod = Vec3f::dot(dir_world, dir_world_proj_ground);
+	float cos_angle = dot_prod / (dir_world.length() * dir_world_proj_ground.length());
+	float elevation_sign = (dir_world.y < 0.0f) ? -1.0f : 1.0f;
+	float elevation_angle_rad = std::acos(cos_angle) * elevation_sign;
+
+	// NOTE: Помним, что мы работаем в плоскости XZ а не XY.
+	// Также учитываем, что в нашей СК положительный Z смотрит в камеру, а отрицательный - это север,
+	// от которого по часовой идёт положительное вращение, поэтому 'pi_half +' а не 'pi_half -'.
+	float azimuth_angle_rad = pi_half + std::atan2(dir_world_proj_ground.z, dir_world_proj_ground.x);
+	if (azimuth_angle_rad < 0.0f)
+		azimuth_angle_rad = pi_two + azimuth_angle_rad;
+	if (std::abs(azimuth_angle_rad) <= epsilon)
+		azimuth_angle_rad = 0.0f;
+
+	antenna_orientation.elevation_deg = elevation_angle_rad * rad_to_deg;
+	antenna_orientation.azimuth_deg = azimuth_angle_rad * rad_to_deg;
 }
 
-void draw_node0(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer) {
-	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f, 0.0f });
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
-
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * translation);
-}
-
-void draw_node1(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles) {
-	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f + 0.5f, 0.0f });
-	const Mat4f yaw = Mat4f::create_rotation_y(input_angles.yaw_angle_rad);
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
-
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw * translation);
-}
-
-void draw_node2(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles) {
-	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, -2.0f + 0.5f, 0.0f });
-	const Mat4f yaw = Mat4f::create_rotation_y(input_angles.yaw_angle_rad);
-	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
-
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw * translation * tilt);
-}
-
-void draw_node3(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles) {
-	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, 2.7f, -0.8f });
-	const Mat4f translation2 = Mat4f::create_translation(Vec3f{ 0.0f, -2.0 + 0.5f, 0.0f });
-	const Mat4f yaw = Mat4f::create_rotation_y(input_angles.yaw_angle_rad);
-	const Mat4f roll = Mat4f::create_rotation_z(input_angles.roll_angle_rad);
-	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
-
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw * translation2 * tilt * translation * roll);
-}
-
-void draw_node4(std::vector<Polygon>& polygons, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const InputAngles& input_angles) {
-	const Mat4f translation = Mat4f::create_translation(Vec3f{ 0.0f, 2.7f, -1.3f });
-	const Mat4f translation2 = Mat4f::create_translation(Vec3f{ 0.0f, -2.0 + 0.5f, 0.0f });
-	const Mat4f yaw_rot = Mat4f::create_rotation_y(input_angles.yaw_angle_rad);
-	const Mat4f roll = Mat4f::create_rotation_z(input_angles.roll_angle_rad);
-	const Mat4f tilt = Mat4f::create_rotation_x(10.0f * deg_to_rad);
-	const Mat4f pitch = Mat4f::create_rotation_x(input_angles.pitch_angle_rad);
-	const Mat4f rotation_y = Mat4f::create_rotation_y(60.0f * deg_to_rad);
-	const Mat4f rotation_x = Mat4f::create_rotation_x(20.0f * deg_to_rad);
-
-	rasterize_polygons_flat_shaded_ortho(polygons, light, framebuffer, z_buffer, rotation_x * rotation_y * yaw_rot * translation2 * tilt * translation * roll * pitch);
+void draw_antenna(std::vector<std::vector<Polygon>> antenna_mesh, const Light& light, Framebuffer& framebuffer, ZBuffer& z_buffer, const TransformMatrices& transforms) {
+	rasterize_polygons_flat_shaded_ortho(antenna_mesh[0], light, framebuffer, z_buffer, transforms.transform_node0);
+	rasterize_polygons_flat_shaded_ortho(antenna_mesh[1], light, framebuffer, z_buffer, transforms.yaw_trans_cam);
+	rasterize_polygons_flat_shaded_ortho(antenna_mesh[2], light, framebuffer, z_buffer, transforms.tilt_yaw_trans_cam);
+	rasterize_polygons_flat_shaded_ortho(antenna_mesh[3], light, framebuffer, z_buffer, transforms.tilt_yaw_trans_cam * transforms.translation_node3 * transforms.roll);
+	rasterize_polygons_flat_shaded_ortho(antenna_mesh[4], light, framebuffer, z_buffer, transforms.tilt_yaw_trans_cam * transforms.translation_node4 * transforms.pitch_roll);
 }
